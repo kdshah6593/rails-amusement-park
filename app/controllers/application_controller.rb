17010e8ade4f0665e382deeca256dfad2789a7df
Kunal <kdshah6593@gmail.com>
2021-01-27 19:01:06 -0500

    commit tree 4dc654bd2e6cf0a2aabff29e75a2c947951b7a76

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :logged_in
+  helper_method :current_user
+  helper_method :admin_user
+  
+  def logged_in
+    if session[:user_id].nil?
+      redirect_to "/"
+    end
+  end
+
+  def current_user
+    user = User.find_by(id: session[:user_id])
+  end
+
+  def admin_user
+    if !current_user.admin
+      redirect_to attractions_path
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/users/new' => 'users#new'
+  post '/users' => 'users#create'
+  root 'static#home'
+  get '/users/:id' => 'users#show', as: "user"
+  get '/signin' => 'sessions#new'
+  post '/session' => 'sessions#create', as: "session"
+  delete '/session' => 'sessions#destroy', as: "signout"
+  get '/attractions' => 'attractions#index'
+  get '/attractions/new' => 'attractions#new', as: "new_attraction"
+  get '/attractions/:id' => 'attractions#show', as: "attraction"
+  post '/attractions' => 'attractions#create'
+  get '/attractions/:id/edit' => 'attractions#edit', as: "edit_attraction"
+  patch '/attractions/:id' => 'attractions#update'
+  post '/rides' => "rides#create", as: "rides"
+
 end
